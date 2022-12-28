c7555f005169485bdc6d20ce3c65c1ac21c608ff
Ramazan Alipov <ram.alipov@yandex.ru>
2022-12-29 00:07:08 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 200;
+        int milePrice = 20;
+
+        System.out.println((ticketPrice / milePrice) + "миль");
+
+    }
+}
+
